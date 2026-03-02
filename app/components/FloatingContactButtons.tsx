89d49c27ca0c9f399c37967c2e0d47aec5ee1e77
Vijendra2016 <vsisodiyadesign@gmail.com>
2026-03-02 19:24:18 +0530

    remove the gap

---
+++ app/components/FloatingContactButtons.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { useState } from 'react';
+import Link from 'next/link';
+import { Phone, Video } from 'lucide-react';
+
+export default function FloatingContactButtons() {
+  const [openItem, setOpenItem] = useState<'call' | 'location' | null>(null);
+
+  const toggle = (item: 'call' | 'location') => {
+    setOpenItem(openItem === item ? null : item);
+  };
+
+  return (
+<div className="fixed right-1 top-1/2 -translate-y-1/2 z-50 flex flex-col gap-1 pointer-events-auto">
+
+      {/* CALL */}
+      <div
+        className={`flex items-center justify-end rounded-full bg-[#62f6b5] shadow-lg cursor-pointer overflow-hidden transition-all duration-300
+        ${openItem === 'call' ? 'w-full' : 'w-10 ml-auto'}`}
+        onClick={() => toggle('call')}
+      >
+        <div className="w-10 h-10 flex items-center justify-center shrink-0">
+          <Phone className="text-black  w-5 h-5" />
+        </div>
+
+        {openItem === 'call' && (
+         <Link
+  href="tel:+12487709617"
+  className="text-black font-light pr-5 whitespace-nowrap"
+>
+  Connect Over Phone
+</Link>
+        )}
+      </div>
+
+      {/* LOCATION */}
+      <div
+        className={`flex items-center justify-end rounded-full bg-[#62f6b5] shadow-lg cursor-pointer overflow-hidden transition-all duration-300
+        ${openItem === 'location' ? 'w-full' : 'w-10 ml-auto'}`}
+        onClick={() => toggle('location')}
+      >
+        <div className="w-10 h-10 flex items-center justify-center shrink-0">
+          < Video  className="text-black w-5 h-5" />
+        </div>
+
+        {openItem === 'location' && (
+          <Link
+            href="https://thegoldsmithsbench.bloom.io/schedule"
+            target="_blank"
+            className="text-black font-light pr-5 whitespace-nowrap"
+          >
+            Book a Discovery Call
+          </Link>
+        )}
+        
+      </div>
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import FloatingContactButtons from "./components/FloatingContactButtons";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,7 +30,11 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        {children}
+         <main className="w-full">{children}
+
+           <FloatingContactButtons />
+        </main> 
+        
         <Footer/>
       </body>
     </html>

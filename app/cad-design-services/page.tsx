250b674068bf47ec71efddfef36decb25e370890
Vijendra2016 <vsisodiyadesign@gmail.com>
2026-03-02 18:56:12 +0530

    remove the gap

--- app/cad-design-services/page.tsx
+++ app/cad-design-services/page.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
-import HeroImageSection from "../components/Heroimage";
+import CADHeroImageSection from "./HeroImageSection";
 import ShowcaseSection from "../components/ShowcaseSection";
 export default function Home() {
   return (
    
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       
-      <main className="flex min-h-screen w-full  flex-col items-center justify-between py-2 px-16 bg-white dark:bg-black sm:items-start">
-       <HeroImageSection/>
+      <main className="flex min-h-screen w-full  flex-col items-center justify-between py-2  bg-white dark:bg-black sm:items-start">
+       <CADHeroImageSection/>
        <ShowcaseSection
       cards={[
         {

01ae78318d92e6f85e3cdd1986b1154da92a45c5
Vijendra2016 <vsisodiyadesign@gmail.com>
2026-03-03 11:03:52 +0530

    remove the gap

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
    
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans bg-white">
       
-      <main className="flex min-h-screen w-full  flex-col items-center justify-between py-2 px-2 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full  flex-col items-center justify-between py-2 px-2 bg-white  sm:items-start">
        <HeroImageSection/>
        <ShowcaseSection
       cards={[

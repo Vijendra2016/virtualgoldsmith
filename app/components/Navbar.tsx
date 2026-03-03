5ea079e8bf6bea1c865c2e1b079fa2ff9838efaf
Vijendra2016 <vsisodiyadesign@gmail.com>
2026-03-03 10:35:17 +0530

    remove the gap

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -17,8 +17,8 @@ export default function Navbar() {
   return (
     <>
       {/* Announcement Bar */}
-      <div className="w-full bg-black text-white">
-        <div className="max-w-7xl mx-auto px-6 py-2 text-center text-xl">
+      <div className="w-full bg-[#c5d8de] text-black">
+        <div className="max-w-7xl mx-auto px-1 py-2 text-center text-lg">
           Crafted digitally. Finished by hand. Made to endure.
         </div>
       </div>

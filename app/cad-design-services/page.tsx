f293910832959017aee7e0227114d03ad1d0897d
Vijendra2016 <vsisodiyadesign@gmail.com>
2026-03-03 11:02:09 +0530

    remove the gap

--- app/cad-design-services/page.tsx
+++ app/cad-design-services/page.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
    
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans bg-white">
       
       <main className="flex min-h-screen w-full  flex-col items-center justify-between py-2  bg-white dark:bg-black sm:items-start">
        <CADHeroImageSection/>
